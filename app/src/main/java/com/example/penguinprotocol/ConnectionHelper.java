91dd790f3ac5df1d62ae2803756444961f8b40be
arianajansma <ajansma@zagmail.gonzaga.edu>
2021-10-16 15:28:35 -0700

    connection failing

--- app/src/main/java/com/example/penguinprotocol/ConnectionHelper.java
+++ app/src/main/java/com/example/penguinprotocol/ConnectionHelper.java
@@ -1,5 +1,41 @@
 package com.example.penguinprotocol;
 
-public class ConnectionHelper {
+import android.util.Log;
 
+import org.json.JSONTokener;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Scanner;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class ConnectionHelper {
+    private static final String TAG = "MainActivity";
+
+    //147.222.70.33
+    private String databaseURL = "147.222.70.33/get-table-names";
+
+    public ConnectionHelper() throws Exception{
+        URL url = new URL("http://147.222.70.33/");
+        Object newobj = url.getContent();
+        HttpsURLConnection conn = (HttpsURLConnection)url.openConnection();
+        Log.d(TAG, "SUCCESS");
+        //conn.setRequestMethod("GET");
+        //conn.connect();
+
+        /*
+        InputStream is = new URL(databaseURL).openStream();
+        Log.d(TAG, "Success");
+        is.close();
+
+         */
+    }
 }
