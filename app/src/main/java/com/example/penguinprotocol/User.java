613ab8707bc3f36479db0b5b1292308f036d2106
Jaymin <jayminwest@gmail.com>
2021-10-16 11:13:06 -0700

    worked on review class

--- app/src/main/java/com/example/penguinprotocol/User.java
+++ app/src/main/java/com/example/penguinprotocol/User.java
@@ -1,4 +1,10 @@
 package com.example.penguinprotocol;
 
 public class User {
+    private Program program;
+    private String name;
+    private String gradSemester;
+    private String semAttended;
+
+
 }
