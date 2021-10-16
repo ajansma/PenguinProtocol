6cb2b986bdee88d140acd5b7ed785e65de551938
arianajansma <ajansma@zagmail.gonzaga.edu>
2021-10-16 11:04:01 -0700

    Json simple file added

---
+++ app/src/main/java/com/example/penguinprotocol/User.java
@@ -0,0 +1,19 @@
+package com.example.penguinprotocol;
+import org.json.simple.JSONObject;
+
+public class User {
+    String name;
+    String programName;
+    String userType;
+
+    public User(){
+
+    }
+
+    public void convertInfoToJSON(){
+
+    }
+
+
+
+}
