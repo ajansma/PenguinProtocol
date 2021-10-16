e9cca9701f906cb97b14940621afd08b6e739f9a
arianajansma <ajansma@zagmail.gonzaga.edu>
2021-10-16 15:35:19 -0700

    trying to merge

--- app/src/main/java/com/example/penguinprotocol/MainActivity.java
+++ app/src/main/java/com/example/penguinprotocol/MainActivity.java
@@ -1,42 +1,29 @@
-
 package com.example.penguinprotocol;
 
-import androidx.annotation.LongDef;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 
-<<<<<<< HEAD
-public class MainActivity extends AppCompatActivity {
-    private static final String TAG = "MainActivity";
-=======
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements RecyclerViewAdapter.OnItemListener {
+/**
+ * MainActivity Represents the first page of the app that has all of the Location Names
+ * Implements onItemListener interface to detect for clicks on items in the recycler view
+ */
+public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
 
-    private ArrayList<String> countryNames = new ArrayList<>();
->>>>>>> a542bac31a9c0f3bffa8d6fd605a2488a16c2e63
+    private ArrayList<String> locationNames = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-<<<<<<< HEAD
-        Log.d(TAG, "HELLO");
-        try {
-            ConnectionHelper connect = new ConnectionHelper();
-        } catch (Exception e) {
-            e.printStackTrace();
-            String error = "ERROR";
-            Log.d(TAG, error);
-        }
-=======
         Log.d(TAG, "onCreate: started");
 
         initCountryNames();
@@ -44,9 +31,9 @@ public class MainActivity extends AppCompatActivity implements RecyclerViewAdapt
 
     private void initCountryNames() {
         //This will be populated with the values from the database using loop
-        countryNames.add("Italy");
-        countryNames.add("Spain");
-        countryNames.add("Denmark");
+        locationNames.add("Italy");
+        locationNames.add("Spain");
+        locationNames.add("Denmark");
 
         initCountryRecyclerView();
     }
@@ -54,14 +41,8 @@ public class MainActivity extends AppCompatActivity implements RecyclerViewAdapt
     private void initCountryRecyclerView() {
         Log.d(TAG, "initRecyclerView: init recyclerView");
         RecyclerView recyclerView = findViewById(R.id.ProgramsRecyclerView);
-        RecyclerViewAdapter adapter = new RecyclerViewAdapter(this, countryNames, this);
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(this, locationNames);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-    }
-
-    @Override
-    public void onItemClick(int position) {
-
->>>>>>> a542bac31a9c0f3bffa8d6fd605a2488a16c2e63
     }
 }
