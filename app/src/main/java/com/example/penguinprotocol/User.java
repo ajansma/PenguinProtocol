f62b336da5648ddae92fa9e9dacf36e6c18b1002
arianajansma <ajansma@zagmail.gonzaga.edu>
2021-10-16 11:08:51 -0700

    user fix

--- app/src/main/java/com/example/penguinprotocol/User.java
+++ app/src/main/java/com/example/penguinprotocol/User.java
@@ -1,19 +1,4 @@
 package com.example.penguinprotocol;
-import org.json.simple.JSONObject;
 
 public class User {
-    String name;
-    String programName;
-    String userType;
-
-    public User(){
-
-    }
-
-    public void convertInfoToJSON(){
-
-    }
-
-
-
 }
