9e2f64cee031863041ac869af25d8ac35a150643
arianajansma <ajansma@zagmail.gonzaga.edu>
2021-10-16 21:24:47 -0700

    getReview fixed

--- app/src/main/java/com/example/penguinprotocol/AppController.java
+++ app/src/main/java/com/example/penguinprotocol/AppController.java
@@ -35,7 +35,7 @@ public class AppController {
     }
 
     public ArrayList<Review> getReviews(String Location){
-       String payload = String.format("{\"sel\": \"%s\",\"table\": \"%s\",\"where\": \"%s\"}", "*", "LOCATION", "\"location =" + Location + "\"");
+       String payload = String.format("{\"sel\": \"%s\",\"table\": \"%s\",\"where\": \"%s\"}", "*", "LOCATION", "\"location =\\\"" + Location + "\\\"");
        System.out.println("PAYLOAD" + payload);
        String jsonString = connection.makeRequest("http://147.222.70.33","/basic-query", "POST", "{\"sel\": \"*\", \"table\": \"LOCATION\", \"where\": \"" + Location + "\"");
         try {
